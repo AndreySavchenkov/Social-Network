84e4765c853ce1f64f6cb5ec92d16d24f3d7e7cb
sandvik16 <sannndvik16@gmail.com>
2021-07-03 16:53:50 +0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI5vFX9tv64JjrobsjPi_62hz5WBWU0FXjfA&usqp=CAU"
+                alt=""/>
+        </header>
+    );
+}
+
+export default Header;
