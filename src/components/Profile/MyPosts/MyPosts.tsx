e7011e535670ed879389e2a25fd53ee182b09863
sandvik16 <sannndvik16@gmail.com>
2021-07-04 16:19:40 +0300

    use props

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,6 +3,7 @@ import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+    debugger
     return (
 
         <div>
@@ -12,8 +13,8 @@ const MyPosts = () => {
                 New post
             </div>
             <div className={s.posts}>
-                <Post/>
-                <Post/>
+                <Post message='Hi, how are you?' likesCount='15'/>
+                <Post message="It's my first post!" likesCount='20'/>
             </div>
         </div>
 
--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import s from './Post.module.css';
 
-const Post = () => {
+type postPropsType = {
+    message:string,
+    likesCount: string
+}
+
+const Post = (props:postPropsType) => {
+
     return (
         <div className={s.item}>
             <img src="https://www.vokrug.tv/pic/news/5/f/c/2/rsz300x300_5fc2879465129c11d65749ab9e3db7cc.jpg" alt=""/>
-            post 1
+            {props.message}
             <div>
-                <span>like</span>
+                <span>like {props.likesCount}</span>
             </div>
-
         </div>
     );
 }
