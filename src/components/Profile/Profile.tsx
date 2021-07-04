2b3c493fc7ddf4638f4514c81485df6cf6763c24
sandvik16 <sannndvik16@gmail.com>
2021-07-04 20:48:38 +0300

    added Dialogs component

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,7 +4,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return(
-        <div className={s.content}>
+        <div>
             <div>
                 <img src="https://images.unsplash.com/photo-1500622944204-b135684e99fd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJhbHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80" alt=""/>
             </div>
