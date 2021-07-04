1ce9774405c2aea7ab09cb07d9a3f2251b8e2c3e
sandvik16 <sannndvik16@gmail.com>
2021-07-04 09:40:38 +0300

    added module css

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={s.content}>
             <div>
                 <img
                     src="https://566259-1826367-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2018/01/turkey-3048299_1920-1366x550.jpg"
@@ -12,15 +13,15 @@ const Profile = () => {
                 ava + description
             </div>
             <div>
-                My postss
+                My posts
                 <div>
                     New post
                 </div>
-                <div>
-                    <div>
+                <div className={s.posts}>
+                    <div className={s.item}>
                         post 1
                     </div>
-                    <div>
+                    <div className={s.item}>
                         post 2
                     </div>
                 </div>
