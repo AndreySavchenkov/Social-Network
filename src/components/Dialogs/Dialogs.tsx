187ff15a4b3bf5b5c656393a75a73dd16b5841be
sandvik16 <sannndvik16@gmail.com>
2021-07-05 20:36:24 +0300

    added dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,8 +4,43 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props: any) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogs_items}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Dimych
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Alexej
+                </div>
+                <div className={s.dialog}>
+                    Ira
+                </div>
+                <div className={s.dialog}>
+                    Marat
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    <div className={s.dialog}>
+                        Hi
+                    </div>
+                    <div className={s.dialog}>
+                        How is You?
+                    </div>
+                    <div className={s.dialog}>
+                        I'm fine! )
+                    </div>
+                    <div className={s.dialog}>
+                        Let's go!
+                    </div>
+                    <div className={s.dialog}>
+                        I want your money )
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
