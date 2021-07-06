699d47d4153a8655ec9825ec0db4aab913b445d5
sandvik16 <sannndvik16@gmail.com>
2021-07-06 22:00:49 +0300

    use method array - map

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -32,7 +32,7 @@ const Message = (props: messagePropsType) => {
 
 const Dialogs = (props: any) => {
 
-    let dialogsData = [
+    let dialogs = [
         {id: 1, name: 'Dimuch'},
         {id: 2, name: 'Andrey'},
         {id: 3, name: 'Zina'},
@@ -40,7 +40,7 @@ const Dialogs = (props: any) => {
         {id: 5, name: 'Marat'},
     ];
 
-    let messagesData = [
+    let messages = [
         {id: 1, message: 'Hi'},
         {id: 2, message: 'How are you?'},
         {id: 3, message: 'I want to work'},
@@ -48,23 +48,16 @@ const Dialogs = (props: any) => {
         {id: 5, message: "It's god!"},
     ];
 
+    let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
+    let messagesElements = messages.map(m => <Message message={m.message}/>)
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogs_items}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
-                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
-                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
+                {dialogsElements}
             </div>
             <div className={s.messages}>
-                <Message message={messagesData[0].message}/>
-                <Message message={messagesData[1].message}/>
-                <Message message={messagesData[2].message}/>
-                <Message message={messagesData[3].message}/>
-                <Message message={messagesData[4].message}/>
-
-
+                {messagesElements}
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -4,13 +4,15 @@ import Post from "./Post/Post";
 
 const MyPosts = () => {
 
-    let postData = [
+    let posts = [
         {id: 1, message: "Hello everybody!!!)", likesCount: 12},
         {id: 2, message: "Yeah!", likesCount: 16},
         {id: 3, message: "Zzzzz...", likesCount: 2},
         {id: 4, message: "You're interesting man but I want to sleep", likesCount: 10},
         {id: 5, message: "It's my first post", likesCount: 22},
     ]
+
+    let postsElements = posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
 
     return (
 
@@ -23,12 +25,7 @@ const MyPosts = () => {
                 <h3>New post</h3>
             </div>
             <div className={s.posts}>
-                <Post message={postData[0].message} likesCount={postData[0].likesCount}/>
-                <Post message={postData[1].message} likesCount={postData[1].likesCount}/>
-                <Post message={postData[2].message} likesCount={postData[2].likesCount}/>
-                <Post message={postData[3].message} likesCount={postData[3].likesCount}/>
-                <Post message={postData[4].message} likesCount={postData[4].likesCount}/>
-
+                {postsElements}
             </div>
         </div>
 
