584dc7f9927e3ec46cab9b17dac2940949921194
sandvik16 <sannndvik16@gmail.com>
2021-11-04 20:07:34 +0300

    setState in users component

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -3,14 +3,55 @@ import styles from './users.module.scss'
 import {UsersPropsType} from "./UsersContainer";
 
 export const Users = (props: UsersPropsType) => {
+
+    if (props.users.length === 0) {
+        props.setUsers([
+            {
+                id: 1,
+                photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGb2pAloeJoZ0pYDp0TtfKNvjydz1Fbfq-8g&usqp=CAU',
+                fullName: "Andrey",
+                status: 'I am a boss',
+                followed: true,
+                location: {country: 'Belarus', city: 'Grodno',}
+            },
+            {
+                id: 2,
+                photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGb2pAloeJoZ0pYDp0TtfKNvjydz1Fbfq-8g&usqp=CAU',
+                fullName: "Sasha",
+                status: 'I am a boss too',
+                followed: false,
+                location: {country: 'Russia', city: 'Moscow',}
+            },
+            {
+                id: 3,
+                photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGb2pAloeJoZ0pYDp0TtfKNvjydz1Fbfq-8g&usqp=CAU',
+                fullName: "Masha",
+                status: 'I am a boss too',
+                followed: true,
+                location: {country: 'Poland', city: 'Warsaw',}
+            },
+            {
+                id: 4,
+                photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGb2pAloeJoZ0pYDp0TtfKNvjydz1Fbfq-8g&usqp=CAU',
+                fullName: "Dima",
+                status: 'I am a boss too',
+                followed: false,
+                location: {country: 'Ukraine', city: 'Kiev',}
+            },
+
+        ])
+    }
+
+
     return <div>
         {props.users.map(u => <div key={u.id}>
             <span>
                 <div>
-                    <img src={u.photoUrl} className={styles.photo}/>
+                    <img src={u.photoUrl} className={styles.photo} alt={'avatar'}/>
                 </div>
                 <div>
-                    <button>Follow</button>
+                    {u.followed ? <button onClick={() => props.unfollow(u.id)}>Unfollow</button>
+                        : <button onClick={() => props.follow(u.id)}>Follow</button>}
                 </div>
             </span>
             <span>
--- src/redux/usersReducer.tsx
+++ src/redux/usersReducer.tsx
@@ -28,41 +28,7 @@ export type userLocationType = {
 }
 
 let initialState: usersType = {
-    users: [
-        {
-            id: 1,
-            photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGb2pAloeJoZ0pYDp0TtfKNvjydz1Fbfq-8g&usqp=CAU',
-            fullName: "Andrey",
-            status: 'I am a boss',
-            followed: true,
-            location: {country: 'Belarus', city: 'Grodno',}
-        },
-        {
-            id: 2,
-            photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGb2pAloeJoZ0pYDp0TtfKNvjydz1Fbfq-8g&usqp=CAU',
-            fullName: "Sasha",
-            status: 'I am a boss too',
-            followed: false,
-            location: {country: 'Russia', city: 'Moscow',}
-        },
-        {
-            id: 3,
-            photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGb2pAloeJoZ0pYDp0TtfKNvjydz1Fbfq-8g&usqp=CAU',
-            fullName: "Masha",
-            status: 'I am a boss too',
-            followed: true,
-            location: {country: 'Poland', city: 'Warsaw',}
-        },
-        {
-            id: 4,
-            photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGb2pAloeJoZ0pYDp0TtfKNvjydz1Fbfq-8g&usqp=CAU',
-            fullName: "Dima",
-            status: 'I am a boss too',
-            followed: false,
-            location: {country: 'Ukraine', city: 'Kiev',}
-        },
-
-    ],
+    users: [],
 }
 
 export const usersReducer = (state: usersType = initialState, action: usersReducerActionsTypes) => {
