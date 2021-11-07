5f4a954916b5c67ed3beefd122105ce2fe58c8d4
sandvik16 <sannndvik16@gmail.com>
2021-11-07 15:02:06 +0300

    added function getUsers for creation server request

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -6,15 +6,17 @@ import userPhoto from '../../assets/images/userImage.png'
 
 export const Users = (props: UsersPropsType) => {
 
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then(response => {
-            props.setUsers(response.data.items)
-        })
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => {
+                    props.setUsers(response.data.items)
+                })
+        }
     }
 
-
     return <div>
+        <button onClick={getUsers}>Get Users</button>
         {props.users.map(u => <div key={u.id}>
             <span>
                 <div>
