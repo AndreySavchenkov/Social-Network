949314fbf75371e916c0ca878e47f263cc0e771e
sandvik16 <sannndvik16@gmail.com>
2021-11-28 21:44:06 +0300

    follow-unfollow api

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -3,6 +3,7 @@ import styles from "./users.module.scss";
 import userPhoto from "../../assets/images/userImage.png";
 import {userType} from "../../redux/usersReducer";
 import {NavLink} from "react-router-dom";
+import axios from "axios";
 
 export type UsersType = {
     totalUsersCount: number
@@ -37,13 +38,39 @@ export const Users = (props: UsersType) => {
                 <div>
                     <NavLink to={'/profile/' + u.id}>
                         <img src={u.photos.small !== null ? u.photos.small : userPhoto} className={styles.photo}
-                         alt={'avatar'}/>
+                             alt={'avatar'}/>
                     </NavLink>
 
                 </div>
                 <div>
-                    {u.followed ? <button onClick={() => props.unfollow(u.id)}>Unfollow</button>
-                        : <button onClick={() => props.follow(u.id)}>Follow</button>}
+                    {u.followed ? <button onClick={() => {
+                            axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,{
+                                withCredentials: true,
+                                headers: {
+                                    'API-KEY': 'fd48e7c0-1687-4982-b2f6-85d6861b1aef'
+                                },
+                            })
+                                .then(response => {
+                                    if (response.data.resultCode == 0) {
+                                        props.unfollow(u.id)
+                                    }
+                                })
+
+
+                        }}>Unfollow</button>
+                        : <button onClick={() => {
+                            axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                                withCredentials: true,
+                                headers: {
+                                    'API-KEY': 'fd48e7c0-1687-4982-b2f6-85d6861b1aef'
+                                },
+                            })
+                                .then(response => {
+                                    if (response.data.resultCode == 0) {
+                                        props.follow(u.id)
+                                    }
+                                })
+                        }}>Follow</button>}
                 </div>
             </span>
                     <span>
--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -37,7 +37,10 @@ class UsersContainer extends React.Component<any, any> {
 
     componentDidMount() {
         this.props.toggleIsFetching(true);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,
+            {
+                withCredentials: true,
+            })
             .then(response => {
                 this.props.toggleIsFetching(false);
                 this.props.setUsers(response.data.items)
@@ -48,7 +51,10 @@ class UsersContainer extends React.Component<any, any> {
     onPageChanged = (pageNumber: number) => {
         this.props.toggleIsFetching(true);
         this.props.setCurrentPage(pageNumber)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,
+            {
+                withCredentials: true,
+            })
             .then(response => {
                 this.props.setUsers(response.data.items)
                 this.props.toggleIsFetching(false);
@@ -112,4 +118,4 @@ export default connect(mapStateToProps, {
     setCurrentPage,
     setTotalUsersCount,
     toggleIsFetching,
-    })(UsersContainer)
+})(UsersContainer)
