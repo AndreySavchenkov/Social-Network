98ecd2d3afdcc33c19dd57c09e2d51692893261c
sandvik16 <sannndvik16@gmail.com>
2021-11-29 20:10:08 +0300

    userAPI new layer

---
+++ src/api/api.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: `https://social-network.samuraijs.com/api/1.0/`,
+    withCredentials: true,
+    headers: {
+        'API-KEY': 'fd48e7c0-1687-4982-b2f6-85d6861b1aef'
+    },
+})
+
+export const usersAPI = {
+    getUsers(currentPage:number = 1, pageSize: number = 10)  {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => response.data)
+    }
+}
+
+
+
